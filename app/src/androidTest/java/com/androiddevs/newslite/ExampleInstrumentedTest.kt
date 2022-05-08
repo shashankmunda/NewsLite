6806a6da8f61254c97c413e806001accae0caead
Shashank Munda <2019ugcs026@nitjsr.ac.in>
2022-05-09 02:52:26 +0530

    Reformatted the project structure

--- app/src/androidTest/java/com/androiddevs/newslite/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/androiddevs/newslite/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.androiddevs.mvvmnewsapp
+package com.androiddevs.newslite
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
